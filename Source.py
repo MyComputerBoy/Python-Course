f7a6037a7b575617780e9519e4386c7ee1d4f44b
Kim Chemnitz Chemnitz <hatelag1@gmail.com>
2024-12-15 16:35:04 +0100

    Create Source.py
    Initial Source commit

---
+++ Source.py
@@ -0,0 +1,23 @@
+#This is a comment starting with a #
+
+#To print to the terminal
+print("Hello World!")
+
+#This is a string
+StringVariableName: str = "Hello World!"
+
+#This is how to print a variable to the terminal
+print(StringVariableName)
+
+#This is a number
+IntVariableNameA: int = 127
+IntVariableNameB: int = 255
+
+#This is how to add two numbers and print it to the terminal
+print(IntVariableA + IntVariableB)
+
+#This is how to get an input from the user and writing the prompt
+StringUserInput: str = input("Write a name: ")
+
+#How to write a more complex string to the terminal
+print("This is your name %s." % (StringUserInput))
